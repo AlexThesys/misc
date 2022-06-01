27381ee0e62624a9a208771668849900d7827ec2
alex_%s\n <41104158+AlexThesys@users.noreply.github.com>
2022-06-01 23:44:05 +0300

    Update self_mod_code.cpp

--- self_mod_code.cpp
+++ self_mod_code.cpp
@@ -15,21 +15,20 @@ int main() {
 
     /* Actually we can pass the exact address of the start of the function as the first parameter 
     and the size of the function as the second. */
-    char* foo_addr = (char*)bar;
+    char* bar_addr = (char*)bar;
     SYSTEM_INFO si;
     GetSystemInfo(&si);
     uint64_t page_size = si.dwPageSize;
-    foo_addr = (char*)((uint64_t)foo_addr & ~(page_size - 1));
+    bar_addr = (char*)((uint64_t)bar_addr & ~(page_size - 1));
     DWORD dwOldProtect;
-    if (VirtualProtect(foo_addr, page_size, PAGE_EXECUTE_READWRITE, &dwOldProtect) == -1) {
-        puts("Error while changing page permissions of foo()");
+    if (VirtualProtect(bar_addr, page_size, PAGE_EXECUTE_READWRITE, &dwOldProtect) == -1) {
+        puts("Error while changing page permissions of bar()");
     }
-    // Change the immediate value in the addl instruction in foo() to 42
     unsigned int* instruction = (unsigned int*)((unsigned char*)bar + 0x03);
     const ptrdiff_t offset = line_b - line_a;
     *instruction += offset; // select address of the second string
 
-    if (VirtualProtect(foo_addr, page_size, dwOldProtect, NULL) == -1) {
+    if (VirtualProtect(bar_addr, page_size, dwOldProtect, NULL) == -1) {
         puts("Error while tying to revert page permissions of bar()");
     }
 
