eaacaa5841887859039806695b17bce31d59110a
alex_%s\n <41104158+AlexThesys@users.noreply.github.com>
2024-01-23 12:03:00 +0100

    Update quantize_into_range.cpp

--- quantization/quantize_into_range.cpp
+++ quantization/quantize_into_range.cpp
@@ -19,7 +19,8 @@ typedef uint64_t u64;
 
 #define NUM_ITER 10 
 
-#define NUM_VALS 12 
+#define NUM_VALS 12
+#define NON_ZERO_VALUES 12
 
 #define EPS_4 0.0001f
 #define EPS_5 0.00001f
@@ -59,13 +60,13 @@ union packed_weights_32b {
 union packed_weights_64b {
 	struct {
 		// bits distribution - work in progress
-		u64 x0 : 7;
+		u64 x0 : 8;
 		u64 x1 : 7;
 		u64 x2 : 6;
 		u64 x3 : 6;
 		u64 x4 : 6;
 		u64 x5 : 6;
-		u64 x6 : 6;
+		u64 x6 : 5;
 		u64 x7 : 5;
 		u64 x8 : 5;
 		u64 x9 : 5;
@@ -73,25 +74,6 @@ union packed_weights_64b {
 	};
 
 	u64			u;
-};
-
-union packed_weights_96b {
-	struct {
-		// bits distribution - work in progress
-		u32 x0 : 10;
-		u32 x1 : 10;
-		u32 x2 : 10;
-		u32 x3 : 9;
-		u32 x4 : 9;
-		u32 x5 : 9;
-		u32 x6 : 8;
-		u32 x7 : 8;
-		u32 x8 : 8;
-		u32 x9 : 8;
-		u32 x10 : 7;
-	};
-
-	u32		u[3];
 };
 
 enum max_weight_value {
@@ -140,7 +122,7 @@ inline void pack_weights_64b(const fp32 input[12], u32 size, u64& result) {
 	fp32 range = 1.0f;
 	packed_weights_64b* res = (packed_weights_64b*)&result;
 
-	res->x0 = (u8)roundf(map_into_range<mwv_7_bit>((size > 0) ? input[0] : 0.0f, range)); // maybe just an early exit at the top?
+	res->x0 = (u8)roundf(map_into_range<mwv_8_bit>((size > 0) ? input[0] : 0.0f, range)); // maybe just an early exit at the top?
 	range -= input[0];
 	res->x1 = (u8)roundf(map_into_range<mwv_7_bit>((size > 1) ? input[1] : 0.0f, range));
 	range -= input[1];
@@ -152,7 +134,7 @@ inline void pack_weights_64b(const fp32 input[12], u32 size, u64& result) {
 	range -= input[4];
 	res->x5 = (u8)roundf(map_into_range<mwv_6_bit>((size > 5) ? input[5] : 0.0f, range));
 	range -= input[5];
-	res->x6 = (u8)roundf(map_into_range<mwv_6_bit>((size > 6) ? input[6] : 0.0f, range));
+	res->x6 = (u8)roundf(map_into_range<mwv_5_bit>((size > 6) ? input[6] : 0.0f, range));
 	range -= input[6];
 	res->x7 = (u8)roundf(map_into_range<mwv_5_bit>((size > 7) ? input[7] : 0.0f, range));
 	range -= input[7];
@@ -169,7 +151,7 @@ inline void unpack_weights_64b(fp32 result[12], const u64 input) {
 	fp32 range = 1.0f;
 
 	result[0] = (fp32)qvals.x0;
-	result[0] = map_from_range<mwv_7_bit>(result[0], range);
+	result[0] = map_from_range<mwv_8_bit>(result[0], range);
 	range -= result[0];
 	result[1] = (fp32)qvals.x1;
 	result[1] = map_from_range<mwv_7_bit>(result[1], range);
@@ -187,7 +169,7 @@ inline void unpack_weights_64b(fp32 result[12], const u64 input) {
 	result[5] = map_from_range<mwv_6_bit>(result[5], range);
 	range -= result[5];
 	result[6] = (fp32)qvals.x6;
-	result[6] = map_from_range<mwv_6_bit>(result[6], range);
+	result[6] = map_from_range<mwv_5_bit>(result[6], range);
 	range -= result[6];
 	result[7] = (fp32)qvals.x7;
 	result[7] = map_from_range<mwv_5_bit>(result[7], range);
@@ -206,28 +188,34 @@ inline void unpack_weights_64b(fp32 result[12], const u64 input) {
 }
 
 int main() {
+	static_assert(NUM_VALS >= NON_ZERO_VALUES, "Incorrect input aruments");
+
 	alignas(alignof(__m128))
 	fp32 values[NUM_VALS];
 	fp32 values_b[NUM_VALS];
 	//fp32 values_c[NUM_VALS];
-	fp32 total_accum_orig = 0.0f;
-	fp32 total_accum_fixed = 0.0f;
-	fp32 total_accum_var = 0.0f;
+	fp64 total_accum_orig = 0.0f;
+	fp64 total_accum_fixed = 0.0f;
+	fp64 total_accum_var = 0.0f;
 
 	std::random_device rd;
 	std::mt19937 gen(rd());
 	std::uniform_real_distribution<float> fp_distr(0.0f, 1.0f);
 
 	for (u32 j = 0; j < NUM_ITER; j++) {
-		fp32 accum_orig = 0.0f;
-		fp32 accum_fixed = 0.0f;
-		fp32 accum_var = 0.0f;
-		fp32 total_weight = 0.0f;
+		fp64 accum_orig = 0.0f;
+		fp64 accum_fixed = 0.0f;
+		fp64 accum_var = 0.0f;
+		fp64 total_weight = 0.0f;
 
 		printf("****Iteration #%d****\n\n", j);
 		// generate random values
-		for (int i = 0; i < NUM_VALS; i++) {
+		for (int i = 0; i < NON_ZERO_VALUES; i++) {
 			values[i] = fp_distr(gen);
+			total_weight += values[i];
+		}
+		for (int i = NON_ZERO_VALUES; i < NUM_VALS; i++) {
+			values[i] = 0;
 			total_weight += values[i];
 		}
 
@@ -237,7 +225,7 @@ int main() {
 		puts("==== Original Values ====");
 		for (int i = 0; i < NUM_VALS; i++) {
 			values[i] *= total_weight;
-			accum_orig += values[i];
+			accum_orig += (fp64)values[i];
 			printf("%.6f ", values[i]);
 		}
 
@@ -247,7 +235,7 @@ int main() {
 			fp32 f = values[i];
 			const u8 q = (u8)roundf(f * fp32(mwv_8_bit));
 			f = (fp32)q / mwv_8_bit;
-			accum_fixed += f;
+			accum_fixed += (fp64)f;
 			printf("%.6f ", f);
 		}
 
@@ -260,7 +248,7 @@ int main() {
 		pack_weights_64b(values, NUM_VALS, qval);
 		unpack_weights_64b(values, qval);
 		for (u32 i = 0; i < NUM_VALS; i++) {
-			accum_var += values[i];
+			accum_var += (fp64)values[i];
 			printf("%.6f ", values[i]);
 		}
 
