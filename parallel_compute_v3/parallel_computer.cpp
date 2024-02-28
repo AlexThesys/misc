8274cd013152b7af390e7b82b6046afebb067df4
alex_%s\n <41104158+AlexThesys@users.noreply.github.com>
2024-02-28 15:20:53 +0100

    Update parallel_computer.cpp

--- parallel_compute_v3/parallel_computer.cpp
+++ parallel_compute_v3/parallel_computer.cpp
@@ -1,100 +1,101 @@
 #include "parallel_computer.h"
 
-parallel_computer::parallel_computer() : _stop_workers(false), _thread_counter(0) {
-	const int32_t num_workers = _min((int32_t)platform.features._cpucount - 3, MAX_WORKERS); // temp
+void parallel_computer::initialize() {
+	const s32 num_workers		= _min((s32)platform.features._cpucount - 3, MAX_WORKERS); // temp
 	_workers_sem_feedback.resize(num_workers, nullptr);
 
-	for (int32_t t = 0; t < num_workers; t++) {
-		spawn_thread(worker_func, (void*)this);
+	for (s32 t = 0; t < num_workers; t++) {
+		threading::spawn		(-1, worker_func, "parallel_computer", 0, (void*)this);
 	}
-	//while (_thread_counter != num_workers) {
-	//	_master_sem.wait		();
-	//}
+	while (_thread_counter != num_workers) {
+		_master_sem.wait		();
+	}
 }
 
-parallel_computer::~parallel_computer() {
-	_stop_workers = true;
-	for (auto &sem : _workers_sem_feedback) {
-		sem->signal();
+void parallel_computer::deinitialize() {
+	_stop_workers				= true;
+	for (auto& sem : _workers_sem_feedback) {
+		sem->signal				();
 	}
-	parallel_task task{};
+	parallel_task task			{};
 	while (_task_queue.try_pop_task_queue(task)) {
 		task.completion_sem->signal();
-    	}
-	//while (0 != _thread_counter) {
-	//	_master_sem.wait();
-	//}
+	}
+	while (0 != _thread_counter) {
+		_master_sem.wait		();
+	}
 }
 
 void parallel_computer::worker_func(void *args) {
-	semaphore worker_sem{1};
+	threading::semaphore worker_sem{ 1 };
 
 	parallel_computer *worker_params = (parallel_computer*)args;
 	parallel_task_queue &task_queue = worker_params->_task_queue;
-	const int32_t t_id = atomic_inc(&worker_params->_thread_counter) - 1;
+	const s32 t_id				= threading::inc(&worker_params->_thread_counter) - 1;
 	worker_params->_workers_sem_feedback[t_id] = &worker_sem;
-	volatile int32_t &stop = worker_params->_stop_workers;
-	//worker_params->_master_sem.signal();
+	volatile s32 &stop			= worker_params->_stop_workers;
+	worker_params->_master_sem.signal();
 	while (true) {
 		parallel_task task{};
         while (!(stop) && !task_queue.try_pop_task_queue(task)) {
-			worker_sem.wait();
+			worker_sem.wait		();
         }
         if (stop) {
-		//threading::dec(&worker_params->_thread_counter);
-		//worker_params->_master_sem.signal();
+			threading::dec(&worker_params->_thread_counter);
+			worker_params->_master_sem.signal();
             break;
         }
         // do work
-        task.func_wrapper((void*)task.params, task.st_params);
+        task.func_wrapper		((void*)task.params, task.st_params);
         // signal requester
-        if ((task.num_chunks) == atomic_inc(task.chunk_counter)) {
+        if ((task.num_chunks) == threading::inc(task.chunk_counter)) {
 			task.completion_sem->signal();
         }
     }
 }
 
 void parallel_computer::launch_task_and_wait(task_func *func, void *args, u32 work_size) {
-	vector<parallel_task> tasks;
-	volatile int32_t chunk_counter = 0;
+	u_vector<parallel_task> tasks;
+	volatile s32 chunk_counter	= 0;
 	threading::semaphore completion_sem;
 
-	assign_workload(tasks, work_size, &chunk_counter, args, func, &completion_sem);
-	post_tasks(tasks);
+	assign_workload				(tasks, work_size, &chunk_counter, args, func, &completion_sem);
+	post_tasks					(tasks);
 
-	completion_sem.wait();
+	completion_sem.wait			();
 }
 
-void parallel_computer::assign_workload(vector<parallel_task> &tasks, uint32_t work_size, volatile int32_t *chunk_counter, void *args, task_func *func, semaphore *sem) {
-		const uint32_t workers_size	= _workers_sem_feedback.size();
-		const uint32_t max_threads = _min(workers_size, work_size);
-		const uint32_t block_size = (work_size + max_threads - 1) / max_threads;
-		int32_t tail = work_size - block_size * max_threads;
-		uint32_t offset_accum = 0;
+void parallel_computer::assign_workload(u_vector<parallel_task> &tasks, u32 work_size, volatile s32 *chunk_counter, void *args, task_func *func, threading::semaphore *sem) {
+		const u32 workers_size	= _workers_sem_feedback.size();
+		const u32 max_threads	= _min(workers_size, work_size);
+		const u32 block_size	= (work_size + max_threads - 1) / max_threads;
+		s32 tail				= work_size - block_size * max_threads;
+		u32 offset_accum		= 0;
 		tasks.resize			(max_threads);
-		for (uint32_t i = 0; i < max_threads; i++) {
+		for (u32 i = 0; i < max_threads; i++) {
 			tasks[i].st_params.block_size = block_size + ((tail-- > 0) ? 1 : 0);
 			tasks[i].st_params.offset = offset_accum;
-			offset_accum += tasks[i].st_params.block_size;
+			offset_accum		+= tasks[i].st_params.block_size;
 
 			tasks[i].num_chunks = max_threads;
 			tasks[i].chunk_counter = chunk_counter;
-			tasks[i].params	= args;
+			tasks[i].params		= args;
 			tasks[i].func_wrapper = func;
 			tasks[i].completion_sem = sem;
         }
 }
 
-void parallel_computer::post_tasks(vector<parallel_task> &tasks) {
-    const int32_t num_threads = (int32_t)tasks[0].num_chunks;
-    for (int32_t i = 0; i < num_threads; i++) {
+void parallel_computer::post_tasks(u_vector<parallel_task> &tasks) {
+    const s32 num_threads		= (s32)tasks[0].num_chunks;
+    for (s32 i = 0; i < num_threads; i++) {
          while (!_task_queue.try_push_task_queue(tasks[i])) {
-             puts("Parallel tasks queue is full! Waiting...");
-			 threading::yield(); // disable this on PS5 ?
+             rlog				(LOG_WARNING "[ANIM] Not enough space in the parallel tasks queue!");
+			 threading::yield	(); // disable this on PS5 ?
          }
     }
     // wake up all the workers - we don't know which ones a currently sleeping
     for (auto &sem : _workers_sem_feedback) { // do we need a lock here?
-		sem->signal();
+		sem->signal				();
     }
 }
+
